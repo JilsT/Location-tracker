50d542ee82ef7dcff5784c00a56a8b978453e66d
JilsT <21bcs016@nith.ac.in>
2022-08-22 17:13:18 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const response = require("express");
+const Datastore = require('nedb');
+const { timeStamp } = require("console");
+const NodeGeocoder = require('node-geocoder');
+
+
+const app = express();
+const databaseFile = new Datastore("databaseFile.db");
+
+databaseFile.loadDatabase();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit : "1mb" }));
+
+app.get("/",function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api", function(req, res){
+    databaseFile.find({}, function(err, data){
+        if(err){
+            res.end();
+            return;
+        }
+        res.json(data);
+    })
+});
+
+
+app.post("/api", function(req, res){
+    console.log(req.body);
+    console.log("request received");
+    const data = req.body;
+    const time = Date.now();
+    const mood = data.mood;
+    data.time = time;
+    databaseFile.insert(data);
+    res.json({
+        status : "success",
+        latitude : data.lat,
+        longitude : data.lon,
+        time : time,
+        mood : mood,
+    });
+});
+
+
+app.listen(3000, function(req, res){
+    console.log("Server is now running on port : 3000");
+});
+
+
+
---
+++ public/index.js
@@ -0,0 +1,60 @@
+if ('geolocation' in navigator) {
+    console.log("geolocation is available");
+
+    navigator.geolocation.getCurrentPosition(async function getData(position) {
+        console.log(position);
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        const button = document.getElementById("geo");
+        button.addEventListener('click', async function () {
+            document.getElementById("lat").textContent = lat;
+            document.getElementById("lon").textContent = lon;
+            document.getElementById("city").textContent = cityName;
+
+
+
+            const mood = document.getElementById("mood").elements[0].value;
+            const data = { lat, lon, mood, cityName };
+            const options = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            };
+
+
+
+            const response = await fetch('/api', options)
+            const jsonData = await response.json();
+            console.log(jsonData);
+
+            const response2 = await fetch(`https://us1.locationiq.com/v1/reverse?key=pk.e8d4442f994b3bbd57e364572930ffce&lat=${lat}&lon=${lon}&format=json`);
+            const data2 = await response2.json();
+            var cityName = " ";
+            if (data2.address.state) {
+                cityName = data2.address.state;
+            }
+            else if (data2.address.city) {
+                cityName = data2.address.city;
+            }
+            console.log(cityName);
+            document.getElementById("city").textContent = cityName;
+
+
+
+
+
+            var marker = L.marker([lat, lon]).addTo(map);
+
+            marker.setLatLng([lat, lon]);
+            map.setView([lat, lon], 5)
+
+        });
+    });
+}
+else {
+    console.log("geolocation is  NOT available");
+}
+
+
